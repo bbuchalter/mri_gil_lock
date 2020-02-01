1683c735f2d19846aa2b00c44af89557685b3088
Brian Buchalter <brian.buchalter@gusto.com>
2020-02-01 08:34:40 -0700

    Add c method that doesn't take arguments

--- ext/hold/hold.c
+++ ext/hold/hold.c
@@ -7,9 +7,15 @@ VALUE holdFor(VALUE self, VALUE rubyTimeToHold)
     return rubyTimeToHold;
 }
 
+void holdForTwoSeconds(VALUE self)
+{
+    usleep(2000000); // avoid possible collision with ruby sleep functions
+}
+
 void Init_hold()
 {
     VALUE moduleMriGilLock = rb_define_module("MriGilLock");
     VALUE classHold = rb_define_class_under(moduleMriGilLock, "Hold", rb_cObject);
     rb_define_singleton_method(classHold, "for_microseconds", holdFor, 1);
+    rb_define_singleton_method(classHold, "for_two_seconds", holdForTwoSeconds, 0);
 }
--- spec/mri_gil_lock_spec.rb
+++ spec/mri_gil_lock_spec.rb
@@ -1,16 +1,25 @@
 require 'benchmark'
 
-RSpec.describe MriGilLock do
-  it "has a version number" do
-    expect(MriGilLock::VERSION).not_to be nil
+RSpec.describe MriGilLock::Hold do
+  describe "#for_microseconds" do
+    it "can hold the GIL for a specified amount of time" do
+      # https://blog.dnsimple.com/2018/03/elapsed-time-with-ruby-the-right-way/
+      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      MriGilLock::Hold.for_microseconds(1_000_000)
+      end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      elasped_time = end_time - start_time
+      expect(elasped_time).to be_between(1.0, 1.1)
+    end
   end
 
-  it "can hold the GIL" do
-    # https://blog.dnsimple.com/2018/03/elapsed-time-with-ruby-the-right-way/
-    start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    MriGilLock::Hold.for_microseconds(1_000_000)
-    end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    elasped_time = end_time - start_time
-    expect(elasped_time).to be_between(1.0, 1.1)
+  describe "#for_two_seconds" do
+    it "can hold the GIL for two seconds" do
+      # https://blog.dnsimple.com/2018/03/elapsed-time-with-ruby-the-right-way/
+      start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      MriGilLock::Hold.for_two_seconds
+      end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+      elasped_time = end_time - start_time
+      expect(elasped_time).to be_between(2.0, 2.1)
+    end
   end
 end
